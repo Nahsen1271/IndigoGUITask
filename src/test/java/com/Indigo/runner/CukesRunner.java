278eedd40a00229534029a086ab6145714b6966d
Nahsen1271 <nahsenfistikci1977@gmail.com>
2022-02-02 13:53:53 +0300

    Framework is created

---
+++ src/test/java/com/Indigo/runner/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.Indigo.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json", // Fancy rapor için pom'da plugin ister. Maven Verify'da çalışır.
+                "html:target/default-html-reports",// default  rapor için herhangi bir plugine gerek yok. Cukesta çalıştırman gerekiyor.
+                "rerun:target/rerun.txt"},
+        features =  "src/test/resources/features",
+        glue = "com/rocketlab/stepDefinitions",
+        dryRun = false,
+        tags = "@nahsen"
+
+)
+public class CukesRunner {
+}
